616489351b16145fd9d8ae0c1e91b33eff91a753
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2023-07-30 17:20:35 +0300

    implemented access restriction to unauthorized users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 import { SequelizeModule } from '@nestjs/sequelize'
@@ -6,10 +6,15 @@ import { User } from './users.models'
 import { Role } from 'src/roles/roles.models'
 import { UserRoles } from 'src/roles/user-roles.model'
 import { RolesModule } from 'src/roles/roles.module'
+import { AuthModule } from 'src/auth/auth.module'
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User, Role, UserRoles]), RolesModule],
+  imports: [
+    SequelizeModule.forFeature([User, Role, UserRoles]),
+    RolesModule,
+    forwardRef(() => AuthModule),
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
