f21d66120cbf6cd2594f1a2cd162b3d58020ba3b
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2023-08-02 19:35:04 +0300

    implemented access by roles

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -1,22 +1,32 @@
 import {
   CanActivate,
   ExecutionContext,
+  HttpException,
+  HttpStatus,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
 import { JwtService } from '@nestjs/jwt'
 import { Observable } from 'rxjs'
+import { ROLES_KEY } from './roles-auth.decorator'
 
 @Injectable()
-export class JwtAuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+export class RloesGuard implements CanActivate {
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
 
   canActivate(
     context: ExecutionContext
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const req = context.switchToHttp().getRequest()
-
     try {
+      const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+        ROLES_KEY,
+        [context.getHandler(), context.getClass()]
+      )
+      if (!requiredRoles) {
+        return true
+      }
+      const req = context.switchToHttp().getRequest()
       const authHeader = req.headers.authorization
 
       const bearer = authHeader.split(' ')[0]
@@ -29,9 +39,9 @@ export class JwtAuthGuard implements CanActivate {
 
       const user = this.jwtService.verify(token)
       req.user = user
-      return true
+      return user.roles.some((role) => requiredRoles.includes(role.value))
     } catch (e) {
-      throw new UnauthorizedException({ message: 'Unauthorize user' })
+      throw new HttpException('Access denied', HttpStatus.FORBIDDEN)
     }
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,8 @@ import { UsersService } from './users.service'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { User } from './users.models'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { Roles } from 'src/auth/roles-auth.decorator'
+import { RloesGuard } from 'src/auth/roles.guard'
 
 @ApiTags('Users')
 @Controller('users')
@@ -20,6 +22,8 @@ export class UsersController {
   @ApiOperation({ summary: 'Gety all users' })
   @ApiResponse({ status: 200, type: [User] })
   // @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
+  @UseGuards(RloesGuard)
   @Get()
   getAll() {
     return this.usersService.getAllUsers()
