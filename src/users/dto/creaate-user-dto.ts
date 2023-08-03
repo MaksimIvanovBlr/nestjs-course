cdd8ec1c1dac96d50e0cb639739a6e05d2ca8c96
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2023-08-03 15:05:22 +0300

    validation of input data

--- src/users/dto/creaate-user-dto.ts
+++ src/users/dto/creaate-user-dto.ts
@@ -1,8 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsEmail, IsString, Length } from 'class-validator'
 export class CreateUserDto {
   @ApiProperty({ example: 'somemail@mail.com', description: "user's email" })
+  @IsString({ message: 'Must be a string' })
+  @IsEmail({}, { message: 'incorrect email' })
   readonly email: string
 
   @ApiProperty({ example: '123456AsDfg', description: "usesr's password" })
+  @IsString({ message: 'Must be a string' })
+  @Length(4, 16, {
+    message: 'The password must be at least 4 and no more then 16 characters',
+  })
   readonly password: string
 }
